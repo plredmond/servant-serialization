ef9de02e1219f86fca378c932154f33473f7ac06
PLR <51248199+plredmond@users.noreply.github.com>
2023-01-28 14:28:00 -0800

    better error handling for ShowRead parse errors

--- lib/Servant/API/ContentTypes/ShowRead.hs
+++ lib/Servant/API/ContentTypes/ShowRead.hs
@@ -10,6 +10,7 @@ import qualified Data.List.NonEmpty as NonEmpty
 import Control.Monad ((<=<))
 import Data.Text.Lazy (pack, unpack)
 import Data.Text.Lazy.Encoding (encodeUtf8, decodeUtf8')
+import Data.Text.Encoding.Error (UnicodeException(..))
 import Text.Read (readEither)
 
 import Servant.API.ContentTypes
@@ -28,7 +29,22 @@ instance Accept ShowRead where
 instance Show a => MimeRender ShowRead a where
     mimeRender Proxy = encodeUtf8 . pack . show
 
+-- $setup
+-- >>> :set -XOverloadedStrings
+
+-- | Decode UTF-8 data and then with 'Read' instance.
+--
+-- >>> mimeUnrender (Proxy :: Proxy ShowRead) "1e5" :: Either String Double
+-- Right 100000.0
+--
+-- >>> mimeUnrender (Proxy :: Proxy ShowRead) "hello" :: Either String Double
+-- Left "Prelude.read: no parse"
+--
+-- >>> mimeUnrender (Proxy :: Proxy ShowRead) "hello\xc3\x28" :: Either String Double
+-- Left "Data.Text.Internal.Encoding.streamDecodeUtf8With: Invalid UTF-8 stream"
 instance Read a => MimeUnrender ShowRead a where
-    mimeUnrender Proxy = readEither . unpack <=< mapLeft show . decodeUtf8'
+    mimeUnrender Proxy = readEither . unpack <=< mapLeft prettyErr . decodeUtf8'
       where
         mapLeft f = either (Left . f) Right
+        prettyErr (DecodeError err _invalidByteValMaybe) = err
+        prettyErr _ = "unknown error" -- TODO: when 'text' removes deprecated 'EncodeError' constructor, remove this case
