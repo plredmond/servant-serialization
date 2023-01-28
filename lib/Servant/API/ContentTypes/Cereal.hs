3eedaf3c234130590d5f77b3acdc003a23ca50cc
PLR <51248199+plredmond@users.noreply.github.com>
2023-01-28 14:44:18 -0800

    better error handling for cereal

--- lib/Servant/API/ContentTypes/Cereal.hs
+++ lib/Servant/API/ContentTypes/Cereal.hs
@@ -11,7 +11,7 @@ import Servant.API.ContentTypes
 import Data.Serialize
 
 -- | Content-type for instances of the 'Serialize' class in the package
--- "cereal".
+-- "cereal". Trailing garbage is ignored.
 data Cereal
 
 -- | Mime-type using the word "hackage" and the name of the package "cereal".
@@ -21,8 +21,25 @@ instance Accept Cereal where
         , "application" // "vnd.hackage.cereal"
         ]
 
+-- |
+--
+-- >>> mimeRender (Proxy :: Proxy Cereal) (3.14 :: Float)
+-- "@H\245\195"
 instance Serialize a => MimeRender Cereal a where
     mimeRender Proxy = encodeLazy
 
+-- |
+--
+-- >>> let bsl = mimeRender (Proxy :: Proxy Cereal) (3.14 :: Float)
+-- >>> mimeUnrender (Proxy :: Proxy Cereal) bsl :: Either String Float
+-- Right 3.14
+--
+-- >>> mimeUnrender (Proxy :: Proxy Cereal) (bsl <> "trailing garbage") :: Either String Float
+-- Right 3.14
+--
+-- >>> mimeUnrender (Proxy :: Proxy Cereal) "garbage" :: Either String (Float, Float)
+-- Left "Data.Serialize.decodeLazy: too few bytes\nFrom:\tdemandInput\n\n"
 instance Serialize a => MimeUnrender Cereal a where
-    mimeUnrender Proxy = decodeLazy
+    mimeUnrender Proxy = mapLeft ("Data.Serialize.decodeLazy: " ++) . decodeLazy
+      where
+        mapLeft f = either (Left . f) Right
