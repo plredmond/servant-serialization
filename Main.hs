cb1816bc610e99a7a84b1c3e900202ff4de3e752
PLR <51248199+plredmond@users.noreply.github.com>
2023-01-27 21:42:32 -0800

    servant server with endpoints for several built-in serialization types

---
+++ Main.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+import Data.Proxy (Proxy(..))
+import Network.Wai.Handler.Warp (run)
+import Data.Text (Text)
+import GHC.Generics (Generic)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Web.FormUrlEncoded as UrlEncoded (ToForm, FromForm)
+import qualified Data.Aeson as Aeson (ToJSON, FromJSON)
+
+import Servant.API
+import Servant.Server
+
+data Example = Example
+    { slythy :: Bool
+    , momeRaths :: [Either Char Ordering]
+    , grabe :: Int
+    } deriving (Generic)
+
+instance UrlEncoded.FromForm Example
+instance UrlEncoded.ToForm Example
+
+instance Aeson.FromJSON Example
+instance Aeson.ToJSON Example
+
+type TestAPI a
+    -- string types
+    =    "string"   :> ReqBody '[PlainText] String              :> Post '[PlainText] String
+    :<|> "text"     :> ReqBody '[PlainText] Text                :> Post '[PlainText] Text
+    :<|> "bs"       :> ReqBody '[OctetStream] BS.ByteString     :> Post '[OctetStream] BS.ByteString
+    :<|> "bsl"      :> ReqBody '[OctetStream] BSL.ByteString    :> Post '[OctetStream] BSL.ByteString
+    -- builtin serialization
+    :<|> "urlenc"   :> ReqBody '[FormUrlEncoded] a              :> Post '[FormUrlEncoded] a
+    :<|> "json"     :> ReqBody '[JSON] a                        :> Post '[JSON] a
+
+main :: IO ()
+main
+    = run 8080
+    . serve (Proxy @(TestAPI Example))
+    $    return
+    :<|> return
+    :<|> return
+    :<|> return
+    :<|> return
+    :<|> return
